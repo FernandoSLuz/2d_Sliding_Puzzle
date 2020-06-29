e6de79d8e43a58580d93b59693b2ad647f6c58d0
FernandoSLuz <fernandosilvadaluz@gmail.com>
2020-06-29 09:50:24 -0300

    project setup
    setup former setup for the game

---
+++ Assets/_Scritps/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+	public GameObject pieceInfoPrefab;
+	public Transform piecesParent;
+	public Piece[,] pieces = new Piece[3, 3];
+	private void Awake()
+	{
+		instance = this;
+		for (int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j ++){
+				if(i == 2 && j == 2) pieces[i, j] = new Piece(i, j, null);
+				else {
+					GameObject go = Instantiate(pieceInfoPrefab, piecesParent, true);
+					pieces[i, j] = new Piece(i, j, go);
+				}
+			}
+		}
+	}
+	public void SearchAvailableSlot(Piece piece){
+		for(int i = 0; i < 3; i++){ 
+			for(int j = 0; j < 3; j++){ 
+				//if(spaces[i,j] == piece.id){
+				//	//esquerda
+				//	if(checkAvailability(i, j - 1)){ 
+					
+				//	}else if(checkAvailability(i, j + 1)){ 
+					
+				//	}else if (checkAvailability(i-1, j)){ 
+					
+				//	}else if (checkAvailability(i+1, j)){ 
+					
+				//	}
+				//}
+			}
+		}
+	}
+	bool checkAvailability(int row, int collumn){ 
+		if(row >= 0 && row <3 && collumn >=0 && collumn <3){
+			return true;
+			//if(pieces[spaces[row,collumn]].empty){
+			//	return true;
+			//}else{
+			//	return false;
+			//}
+		}else{
+			return false;
+		}
+	}
+}
+[System.Serializable]
+public class Piece{
+	public PieceInfo pieceInfo = null;
+	public int row;
+	public int column;
+	public Piece(int newRow, int newColumn, GameObject pieceInfoObj){
+		row = newRow;
+		column = newColumn;
+		if(pieceInfo != null){
+			pieceInfo = pieceInfo.GetComponent<PieceInfo>();
+
+		}
+	}
+}
---
+++ Assets/_Scritps/PieceInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class PieceInfo : MonoBehaviour
+{
+	public TextMeshProUGUI pieceName;
+	public Piece piece;
+	public Button btn;
+	public void LoadPiece(Piece actualPiece){
+		piece = actualPiece;
+		pieceName = GetComponentInChildren<TextMeshProUGUI>();
+		pieceName.text = (((piece.row * 3) + piece.column)+1).ToString();
+		btn = GetComponent<Button>();
+		btn.onClick.AddListener(() => {
+			GameManager.instance.SearchAvailableSlot(piece);
+		});
+	}
+}
